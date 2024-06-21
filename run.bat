0d8726d3039f707665e348c3deba648bf3ca36a7
Albert <thegoversus@gmail.com>
2024-06-21 11:32:21 +0300

    run bot bat file

---
+++ run.bat
@@ -0,0 +1,28 @@
+@echo off
+
+where python >nul
+if %errorlevel% == 0 (
+    echo --- Python found.
+) else (
+    echo --- Python is not installed.
+    goto :EOF
+)
+
+pip list | findstr selenium >nul
+if %errorlevel% == 0 (
+    echo --- Selenium found.
+    goto :run
+) else (
+    echo --- Selenium is not installed.
+)
+
+set /P choice= "--- Would you like to install selenium right now [y/n] "
+if /I "%choice%" EQU "y" (
+    pip install selenium
+    goto :run
+) else if /I "%choice%" EQU "n" (
+    goto :EOF
+)
+
+:run
+python src\main.py
